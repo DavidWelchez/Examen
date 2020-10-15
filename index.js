2e16d05f2cfb50b42140fd23113009879ac9fb68
David <davidwelchez@gmail.com>
2020-10-15 15:11:31 -0600

    Importar handlebars

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Importar 
 const express = require("express");
 
+//handlebars
+const exphbs = require("express-handlebars");
+
+
+
+
 // Crear un servidor express
 const app = express();
+
+
+// Indicar a express utilizar handlebars 
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+
+
+
+
+
+
 
 // Inicializar el servidor en un puerto en específico
 app.listen(5000, () => {
