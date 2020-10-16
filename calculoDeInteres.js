6c89023d02cdb515402b872013c3d11d14912513
David <davidwelchez@gmail.com>
2020-10-15 21:35:01 -0600

    Finalizacion

---
+++ calculoDeInteres.js
@@ -0,0 +1,41 @@
+// Calcula las cuotas basadas en el método francés
+// couta = (capital * i) / (1 - (i + 1) ^ -n)
+// i = interés
+// n = períodos (en meses)
+exports.calcularMetodo = (capital, tasaInteres, plazo) => {
+    
+    let mes = 0;
+    let interes = 0;
+    let tasaInteresDecimal = tasaInteres / 100;
+    const cuotas = [];
+  let capitalFinal =0;  
+  
+    // Transformar los años en meses
+    plazo = Math.round(plazo * 12);
+  
+    // Calcular la cuota
+   
+  
+    while (mes != plazo) {
+     
+capitalFinal = capital * (1+tasaInteresDecimal) ** -plazo;
+
+capitalFinal = capitalFinal / tasaInteres;
+
+interes = tasaInteres + tasaInteresDecimal;
+      // Actualizar el mes (período)
+      mes++;
+  
+     
+  
+      // Almacenar los valores en el arreglo
+      cuotas.push({
+        mes,
+        interes,
+        capital,
+        capitalFinal,
+      });
+    }
+  
+    return cuotas;
+  };
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 
+// Importar body parser 
+const bodyParser = require("body-parser");
+
+// Importar la función de cálculo de Interes
+const { calcularMetodo } = require("./calculoDeInteres");
 
 
 
@@ -18,11 +23,33 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs")
 
 
+// Habilitar body parse 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
 // Crear  ruta para /
 
 app.get("/", (req, res, next) => {
   res.render("formulario_interes");
 });
+
+/////
+
+app.post("/CalculoInteres", (req, res, next) => {
+  // Asignación por destructuring
+  // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
+  const { monto, tasaInteres, periodo } = req.body;
+
+  const cuotas = calcularMetodo(monto, tasaInteres, periodo);
+
+  res.render("Formulario_resultado", { cuotas });
+});
+
+
+
+
 
 
 
